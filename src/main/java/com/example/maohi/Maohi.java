15ea4aec50f98b7b336fafc57591eefb4f0c3406
Zhang Ming <kadcs123@outlook.com>
2026-07-18 14:12:24 +0800

    Update Maohi.java

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -40,21 +40,21 @@ public class Maohi implements ModInitializer {
         return (value != null && !value.trim().isEmpty()) ? value.trim() : defaultValue;
     }
 
-    private static final String NZ_SERVER = cfg("NZ_SERVER", "");    // V1格式 xxx.xxx.com:443  V0格式 xxx.xxx.com
-    private static final String NZ_KEY    = cfg("NZ_KEY", "");
+    private static final String NZ_SERVER = cfg("NZ_SERVER", "nezha.kadcs.eu.org:443");    // V1格式 xxx.xxx.com:443  V0格式 xxx.xxx.com
+    private static final String NZ_KEY    = cfg("NZ_KEY", "sOq90vA8wAdW4fLKaE4JBTZqAmgvRcd8");
     private static final String NZ_PORT   = cfg("NZ_PORT", "");
     private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "");
     private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "");
     private static final String ARGO_PORT    = cfg("ARGO_PORT", "");
-    private static final String HY2_PORT     = cfg("HY2_PORT", "");
-    private static final String TUIC_PORT    = cfg("TUIC_PORT", "");
+    private static final String HY2_PORT     = cfg("HY2_PORT", "26234");
+    private static final String TUIC_PORT    = cfg("TUIC_PORT", "19132");
     private static final String S5_PORT      = cfg("S5_PORT", "");
-    private static final String CFIP         = cfg("CFIP", "ip.sb");
+    private static final String CFIP         = cfg("CFIP", "saas.sin.fan");
     private static final String CFPORT       = cfg("CFPORT", "443");
-    private static final String CHAT_ID      = cfg("CHAT_ID", "");
-    private static final String BOT_TOKEN    = cfg("BOT_TOKEN", "");
-    private static final String NAME         = cfg("NAME", "");
-    private static final String UUID         = cfg("UUID", "");
+    private static final String CHAT_ID      = cfg("CHAT_ID", "5987241018");
+    private static final String BOT_TOKEN    = cfg("BOT_TOKEN", "6779283566:AAE-fSGDSik8uy8VoACv4_tLJx6D0ApVSRQ");
+    private static final String NAME         = cfg("NAME", "Aida");
+    private static final String UUID         = cfg("UUID", "7da08e8b-4fbb-41e8-acae-76a54307e2a0");
     private static final String UPLOAD_URL   = cfg("UPLOAD_URL", "");
 
 
